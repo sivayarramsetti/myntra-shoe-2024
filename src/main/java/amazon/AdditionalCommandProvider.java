d819e0ea441f21ca0a95f5eb7b6b673c32f3312a
sivanagaraju yarramsetti <sivayarramsetti39@gmail.com>
2024-07-02 14:30:37 +0530

    data done

---
+++ src/main/java/amazon/AdditionalCommandProvider.java
@@ -0,0 +1,72 @@
+package amazon;
+
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.logging.Level;
+
+import schemacrawler.schemacrawler.SchemaCrawlerOptions;
+import schemacrawler.tools.executable.CommandProvider;
+import schemacrawler.tools.executable.Executable;
+import schemacrawler.tools.integration.freemarker.FreeMarkerRenderer;
+import schemacrawler.tools.iosource.ClasspathInputResource;
+import schemacrawler.tools.iosource.InputResource;
+import schemacrawler.tools.iosource.StringInputResource;
+import schemacrawler.tools.options.OutputOptions;
+
+public class AdditionalCommandProvider
+  implements CommandProvider
+{
+
+  @Override
+  public Executable configureNewExecutable(final String command,
+                                           final SchemaCrawlerOptions schemaCrawlerOptions,
+                                           final OutputOptions outputOptions)
+  {
+    if (!AdditionalExecutable.COMMAND.equals(command)) {
+      throw new IllegalArgumentException("Cannot support command, " + command);
+    }
+    
+    final AdditionalExecutable executable = new AdditionalExecutable();
+    if (schemaCrawlerOptions != null)
+    {
+      executable.setSchemaCrawlerOptions(schemaCrawlerOptions);
+    }
+    if (outputOptions != null)
+    {
+      executable.setOutputOptions(outputOptions);
+    }
+    return executable;
+  }
+
+  @Override
+  public InputResource getHelp()
+  {
+    final String helpResource = "/help/AdditionalCommandProvider.txt";
+    try
+    {
+      return new ClasspathInputResource(helpResource);
+    }
+    catch (final IOException e)
+    {
+      // Log error...
+      return new StringInputResource("No help available");
+    }
+  }
+  
+  @Override
+  public Collection<String> getSupportedCommands()
+  {
+    return Arrays.asList(AdditionalExecutable.COMMAND);
+  }
+
+  @Override
+  public boolean supportsCommand(final String command,
+                                 final SchemaCrawlerOptions schemaCrawlerOptions,
+                                 final OutputOptions outputOptions)
+  {
+    return AdditionalExecutable.COMMAND.equals(command);
+  }
+
+}
---
+++ src/main/java/amazon/AdditionalExecutable.java
@@ -0,0 +1,57 @@
+package amazon;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import schemacrawler.schema.Catalog;
+import schemacrawler.schema.Column;
+import schemacrawler.schema.Schema;
+import schemacrawler.schema.Table;
+import schemacrawler.tools.executable.BaseStagedExecutable;
+import sf.util.StringFormat;
+
+public class AdditionalExecutable
+  extends BaseStagedExecutable
+{
+
+  private static final Logger LOGGER = Logger
+    .getLogger(AdditionalExecutable.class.getName());
+
+  static final String COMMAND = "additional";
+
+  protected AdditionalExecutable()
+  {
+    super(COMMAND);
+  }
+
+  @Override
+  public void executeOn(final Catalog catalog, final Connection connection)
+    throws Exception
+  {
+    // TODO: Possibly process command-line options, which are available
+    // in
+    // additionalConfiguration
+
+    try (final PrintWriter writer = new PrintWriter(outputOptions
+      .openNewOutputWriter());)
+    {
+      for (final Schema schema: catalog.getSchemas())
+      {
+        // SchemaCrawler will control output of log messages if you use
+        // JDK logging
+        LOGGER.log(Level.INFO, new StringFormat("Processing <%s>", schema));
+        for (final Table table: catalog.getTables(schema))
+        {
+          writer.println("o--> " + table);
+          for (final Column column: table.getColumns())
+          {
+            writer.println("     o--> " + column);
+          }
+        }
+      }
+    }
+  }
+
+}
